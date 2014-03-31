a79b719452418713f867303f304149d14c311f82
Eric Anger <eanger@gmail.com>
2014-03-31 19:16:32 -0400

    generalize counter process. just specify a counter name and itll count it

--- eaudit.h
+++ eaudit.h
@@ -1,6 +1,8 @@
 #ifndef EAUDIT_H
 #define EAUDIT_H
 
+#include <vector>
+
 void EAUDIT_push();
 void EAUDIT_pop(const char* func_name);
 void EAUDIT_shutdown();
@@ -8,7 +10,7 @@ void EAUDIT_shutdown();
 /*void overflow(int eventset, void* address, long long overflow_vector, 
               void* context);*/
 void overflow(int signum);
-void init_papi(int* eventset);
+void init_papi(std::vector<int>& eventsets);
 bool read_rapl();
 
 #endif // EAUDIT_H
