95609e85a29e1588bbb4bce7225df0ebfada6cc5
Eric Anger <eanger@gmail.com>
2014-03-31 11:13:19 -0400

    functioning sampler

--- eaudit.h
+++ eaudit.h
@@ -3,10 +3,24 @@
 
 #include <signal.h>
 
+struct stats_t{
+  long long package_energy;
+  long long pp0_energy;
+  long long time;
+  stats_t(long long pae, long long ppe, long long t) : package_energy{pae}, pp0_energy{ppe}, time{t} {}
+  stats_t() : package_energy{0}, pp0_energy{0}, time{0} {}
+
+  stats_t& operator+=(const stats_t& rhs){
+    package_energy += rhs.package_energy;
+    pp0_energy += rhs.pp0_energy;
+    time += rhs.time;
+  }
+};
+
 void EAUDIT_shutdown();
 
 void overflow(int signum, siginfo_t* info, void* context);
-void init_papi(int* eventset);
-void read_rapl();
+int init_papi();
+stats_t read_rapl();
 
 #endif // EAUDIT_H
--- test.cpp
+++ test.cpp
@@ -1,15 +1,33 @@
 #include <iostream>
+#include <unistd.h>
+#include <ctime>
+#include <cstdlib>
 
-void baz(){
-  while(1);
+void foo(int n){
+  int* x = (int*) malloc(n * sizeof(int));
+  int* y = (int*) malloc(n * sizeof(int));
+  int* z = (int*) malloc(n * sizeof(int));
+
+  srand(time(NULL));
+
+  for(int i = 0; i < n; ++i){
+    x[i] = rand();
+    y[i] = rand();
+  }
+
+  for(int j = 0; j < 10; ++j){
+    for(int i = 0; i < n; ++i){
+      z[i] = x[i] * y[i];
+    }
+  }
+
+  free(z);
+  free(y);
+  free(x);
 }
 
 void bar(){
-  baz();
-}
-
-void foo(){
-  bar();
+  sleep(2);
 }
 
 int main(){
@@ -18,7 +36,8 @@ int main(){
   int x = 0;
   int y = 1;
   int z = y + x;
-  foo();
+  //foo(1000000);
+  bar();
 
   return 0;
 }
