d51924e2b964501aa522f2178e9aafa39c43960a
Eric Anger <eanger@gmail.com>
2014-11-07 16:49:39 -0500

    Clean up and comment code

--- eaudit.cpp
+++ eaudit.cpp
@@ -98,7 +98,173 @@ stats_t read_rapl(const vector<event_info_t>& eventsets){
 }
 
 
+void do_profiling(int child, char* child_name) {
+  /*
+   * Structures holding profiling data
+   */
+  vector<event_info_t> eventsets;
+  vector<string> counter_names;
+  map<void*, stats_t> stat_map;
+
+  /*
+   * Initialize profiling with PAPI
+   */
+  print("Init PAPI\n");
+  int retval;
+  if ((retval = PAPI_library_init(PAPI_VER_CURRENT)) != PAPI_VER_CURRENT) {
+    fprintf(stderr, "Unable to init PAPI library.\n");
+    switch (retval) {
+      case PAPI_EINVAL:
+        fprintf(stderr, "einval\n");
+        break;
+      case PAPI_ENOMEM:
+        fprintf(stderr, "enomem\n");
+        break;
+      case PAPI_ESBSTR:
+        fprintf(stderr, "esbstr\n");
+        break;
+      case PAPI_ESYS:
+        fprintf(stderr, "esys\n");
+        break;
+    }
+    exit(-1);
+  }
+
+  for (auto& event_name : kCounterNames) {
+    int event_code;
+    retval = PAPI_event_name_to_code((char*)event_name, &event_code);
+    if (retval != PAPI_OK) {
+      PAPI_perror(NULL);
+      exit(-1);
+    }
+    int component = PAPI_get_event_component(event_code);
+    auto elem = find_if(
+        begin(eventsets), end(eventsets),
+        [&](const event_info_t& c) { return c.component == component; });
+    if (elem == end(eventsets)) {
+      event_info_t new_info;
+      new_info.component = component;
+      new_info.eventcodes.push_back(event_code);
+      eventsets.push_back(new_info);
+    } else {
+      elem->eventcodes.push_back(event_code);
+    }
+  }
+
+  for (auto& event : eventsets) {
+    int eventset = PAPI_NULL;
+    PAPI_create_eventset(&eventset);
+    if (retval != PAPI_OK) {
+      PAPI_perror(NULL);
+      exit(-1);
+    }
+    for (const auto& eventcode : event.eventcodes) {
+      char name[PAPI_MAX_STR_LEN];
+      PAPI_event_code_to_name(eventcode, name);
+      counter_names.emplace_back(name);
+    }
+    PAPI_add_events(eventset, &event.eventcodes[0], event.eventcodes.size());
+    if (retval != PAPI_OK) {
+      PAPI_perror(NULL);
+      exit(-1);
+    }
+    event.eventset = eventset;
+    retval = PAPI_start(eventset);
+    if (retval != PAPI_OK) {
+      PAPI_perror(NULL);
+      exit(-1);
+    }
+  }
+
+  /*
+   * Let the child run, periodically interrupting to collect profile data.
+   */
+  print("Start profiling.\n");
+  ptrace(PTRACE_CONT, child, nullptr, nullptr);
+  struct user_regs_struct regs;
+  for (;;) {
+    usleep(kSleepUsecs);
+    kill(child, SIGINT);
+    int status;
+    wait(&status);
+    if (WIFEXITED(status)) {
+      break;
+    }
+    ptrace(PTRACE_GETREGS, child, nullptr, &regs);
+    void* rip = (void*)regs.rip;
+    auto rapl = read_rapl(eventsets);
+    stat_map[rip] += rapl;
+    ptrace(PTRACE_CONT, child, nullptr, nullptr);
+  }
+
+  /*
+   * Done profiling. Convert data to output file.
+   */
+  print("Finalize profile.\n");
+  vector<pair<string, stats_t> > stats;
+  // Convert stack IDs into function names.
+  for (auto& func : stat_map) {
+    stringstream cmd;
+    cmd << "addr2line -f -s -C -e " << child_name << " " << func.first;
+    auto pipe = popen(cmd.str().c_str(), "r");  // call command and read output
+    if (!pipe) {
+      cerr << "Unable to open pipe to call addr2line.\n";
+      return;
+    }
+    char buffer[128];
+    string result = "";
+    while (!feof(pipe)) {
+      if (fgets(buffer, 128, pipe) != nullptr) {
+        result += buffer;
+      }
+    }
+    pclose(pipe);
+    stringstream resultstream{result};
+    string line;
+    getline(resultstream, line);
+
+    auto stat = find_if(
+        begin(stats), end(stats),
+        [&](const pair<string, stats_t>& obj) { return obj.first == line; });
+    if (stat == end(stats)) {
+      stats.emplace_back(line, func.second);
+    } else {
+      stat->second += func.second;
+    }
+  }
+
+  stable_sort(stats.begin(), stats.end(), [](const pair<string, stats_t>& a,
+                                             const pair<string, stats_t>& b) {
+    return a.second.time > b.second.time;
+  });
+
+  /*
+   * Write profile to file
+   */
+  ofstream myfile;
+  myfile.open(kOutFileName);
+  myfile << "Func Name"
+         << "\t"
+         << "Time(s)";
+  for (const auto& name : counter_names) {
+    myfile << "\t" << name;
+  }
+  myfile << endl;
+
+  for (auto& func : stats) {
+    myfile << func.first << "\t" << func.second.time* kNanoToBase;
+    for (int i = 0; i < kNumCounters; ++i) {
+      myfile << "\t" << func.second.counters[i];
+    }
+    myfile << endl;
+  }
+  myfile.close();
+}
+
 int main(int argc, char* argv[]) {
+  /*
+   * Fork a process to run the profiled application
+   */
   auto child = fork();
   if(child > 0){ /* parent */
     // Let's do this.
@@ -108,147 +274,7 @@ int main(int argc, char* argv[]) {
       cerr << "Child exited too fast.\n";
       return 0;
     }
-
-    print("init\n");
-    vector<event_info_t> eventsets;
-    vector<string> counter_names;
-    map<void*, stats_t> stat_map;
-    int retval;
-    if ((retval = PAPI_library_init(PAPI_VER_CURRENT)) != PAPI_VER_CURRENT) {
-      fprintf(stderr, "Unable to init PAPI library.\n");
-      switch (retval) {
-        case PAPI_EINVAL:
-          fprintf(stderr, "einval\n");
-          break;
-        case PAPI_ENOMEM:
-          fprintf(stderr, "enomem\n");
-          break;
-        case PAPI_ESBSTR:
-          fprintf(stderr, "esbstr\n");
-          break;
-        case PAPI_ESYS:
-          fprintf(stderr, "esys\n");
-          break;
-      }
-      exit(-1);
-    }
-
-    for (auto& event_name : kCounterNames) {
-      int event_code;
-      retval = PAPI_event_name_to_code((char*)event_name, &event_code);
-      if (retval != PAPI_OK) {
-        PAPI_perror(NULL);
-        exit(-1);
-      }
-      int component = PAPI_get_event_component(event_code);
-      auto elem = find_if(begin(eventsets), end(eventsets),
-                          [&](const event_info_t& c){
-                            return c.component == component; });
-      if(elem == end(eventsets)){
-        event_info_t new_info;
-        new_info.component = component;
-        new_info.eventcodes.push_back(event_code);
-        eventsets.push_back(new_info);
-      } else {
-        elem->eventcodes.push_back(event_code);
-      }
-    }
-
-    for (auto& event : eventsets) {
-      int eventset = PAPI_NULL;
-      PAPI_create_eventset(&eventset);
-      if (retval != PAPI_OK) {
-        PAPI_perror(NULL);
-        exit(-1);
-      }
-      for(const auto& eventcode : event.eventcodes){
-        char name[PAPI_MAX_STR_LEN];
-        PAPI_event_code_to_name(eventcode, name);
-        counter_names.emplace_back(name);
-      }
-      PAPI_add_events(eventset, &event.eventcodes[0], event.eventcodes.size());
-      if (retval != PAPI_OK) {
-        PAPI_perror(NULL);
-        exit(-1);
-      }
-      event.eventset = eventset;
-      retval = PAPI_start(eventset);
-      if (retval != PAPI_OK) {
-        PAPI_perror(NULL);
-        exit(-1);
-      }
-    }
-    ptrace(PTRACE_CONT, child, nullptr, nullptr);
-    struct user_regs_struct regs;
-    for(;;){
-      usleep(kSleepUsecs);
-      kill(child, SIGINT);
-      wait(&status);
-      if(WIFEXITED(status)){
-        break;
-      }
-      ptrace(PTRACE_GETREGS, child, nullptr, &regs);
-      void* rip = (void*) regs.rip;
-      auto rapl = read_rapl(eventsets);
-      stat_map[rip] += rapl;
-      ptrace(PTRACE_CONT, child, nullptr, nullptr);
-    }
-    print("shutdown\n");
-
-    vector<pair<string, stats_t> > stats;
-    for (auto& func : stat_map) {
-      stringstream cmd;
-      cmd << "addr2line -f -s -C -e " << argv[1] << " " << func.first;
-      auto pipe = popen(cmd.str().c_str(), "r"); // call command and read output
-      if(!pipe) {
-        cerr << "Unable to open pipe to call addr2line.\n";
-        return -1;
-      }
-      char buffer[128];
-      string result = "";
-      while(!feof(pipe)){
-        if(fgets(buffer, 128, pipe) != nullptr){
-          result += buffer;
-        }
-      }
-      pclose(pipe);
-      stringstream resultstream{result};
-      string line;
-      getline(resultstream, line);
-
-      auto stat = find_if(begin(stats), end(stats),
-                          [&](const pair<string, stats_t>& obj){
-                            return obj.first == line;});
-      if(stat == end(stats)){
-        stats.emplace_back(line, func.second);
-      } else {
-        stat->second += func.second;
-      }
-    }
-
-    stable_sort(stats.begin(), stats.end(), [](const pair<string, stats_t>& a,
-                                               const pair<string, stats_t>& b) {
-      return a.second.time > b.second.time;
-    });
-
-    ofstream myfile;
-    myfile.open(kOutFileName);
-    myfile << "Func Name"
-           << "\t"
-           << "Time(s)";
-    for(const auto& name : counter_names){
-      myfile << "\t" << name;
-    }
-    myfile << endl;
-
-    for (auto& func : stats) {
-      myfile << func.first << "\t" << func.second.time* kNanoToBase;
-      for (int i = 0; i < kNumCounters; ++i) {
-        myfile << "\t" << func.second.counters[i];
-      }
-      myfile << endl;
-    }
-    myfile.close();
+    do_profiling(child, argv[1]);
   } else if(child == 0){ /* child */
     // prepare for tracing
     ptrace(PTRACE_TRACEME, 0, nullptr, nullptr);
